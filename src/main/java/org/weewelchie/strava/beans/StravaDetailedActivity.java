c2aa3c29b3bd9230a10976d691b800e1ed405b85
Chris Welch <welchie99@gmail.com>
2025-09-03 15:18:47 +0100

    Updated to remove 'secret'

--- src/main/java/org/weewelchie/strava/beans/StravaDetailedActivity.java
+++ src/main/java/org/weewelchie/strava/beans/StravaDetailedActivity.java
@@ -61,40 +61,4 @@ public class StravaDetailedActivity extends StravaActivity{
 
     @JsonProperty("available_zones")
     private String[] availableZones;
-
-
-    /*
-
-
-    "hide_from_home": false,
-    "device_name": "Garmin Forerunner 235",
-    "embed_token": "9bc3977b809257e4f0b9a2e28bd35d50a7c09c8a",
-    "similar_activities": {
-        "effort_count": 88,
-        "average_speed": 3.0528257491780657,
-        "min_average_speed": 2.779524584800201,
-        "mid_average_speed": 3.100733826513,
-        "max_average_speed": 3.50588297223513,
-        "pr_rank": null,
-        "frequency_milestone": null,
-        "trend": {
-            "speeds": [
-                3.086985259128194,
-                3.0875308941307247,
-                3.050339104903966,
-                3.051473657614121,
-                3.0154740156410833
-            ],
-            "current_activity_index": 3,
-            "min_speed": 2.779524584800201,
-            "mid_speed": 3.100733826513,
-            "max_speed": 3.50588297223513,
-            "direction": 0
-        },
-        "resource_state": 2
-    },
-    "available_zones": []
-}
-     */
-
 }
